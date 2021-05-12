5a7fda47050150a576a454a64c521981a97a3079
Vilasi <VilasiCoding@gmail.com>
2021-05-12 19:50:50 -0400

    Changed orientation of headshot image. Needs updating

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-    $('.materialboxed').materialbox();
-    $('.scrollspy').scrollSpy();
-    $('.parallax').parallax();
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+  $('.materialboxed').materialbox();
+  $('.scrollspy').scrollSpy();
+  $('.parallax').parallax();
 
-    /* ScrollReveal allows the selected class to be revealed as the page is scrolled down*/
-    ScrollReveal().reveal('.scrollReveal');
-
-
-    
-    
-    
-    
-    
+  /* ScrollReveal allows the selected class to be revealed as the page is scrolled down*/
+  ScrollReveal().reveal('.scrollReveal');
 });
 
 /*
@@ -39,7 +32,3 @@ window.onload = (event) => {
 };
 
 */
-
-
-
-
